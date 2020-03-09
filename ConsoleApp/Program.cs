8222c2e6050cf462459dbc5e64808e501c21e9ae
Shay Rojansky <roji@roji.org>
2020-03-09 21:47:15 +0100

    Console app for playing around

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Npgsql;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand("SELECT 1", conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+            }
+        }
+    }
+}
